469d9dc0debebf6a31e02ddf50a4252964576bdf
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-31 23:38:01 +0530

    changes the background color

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function imgSlider(anything){
     document.querySelector('.pepsi').src= anything;
 }
+
+function changeBgColor(color){
+    const section = document.querySelector('.sec')
+    section.style.background = color;
+}

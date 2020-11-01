e08e5fcbaa39d592a667ae65805114e5ef263740
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-01 10:20:53 +0530

    toggle the nav menu

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function changeBgColor(color){
 
 function toggleMenu(){
     const toggleMenu = document.querySelector('.toggleMenu')
+    const navigation = document.querySelector('.navigation')
     toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
 }

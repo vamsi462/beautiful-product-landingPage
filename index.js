8720dae46d0d203f102165573e6fcbb6a295f5c0
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-11-01 10:07:18 +0530

    toggling between menu and close

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function changeBgColor(color){
     const section = document.querySelector('.sec')
     section.style.background = color;
 }
+
+function toggleMenu(){
+    const toggleMenu = document.querySelector('.toggleMenu')
+    toggleMenu.classList.toggle('active')
+}
